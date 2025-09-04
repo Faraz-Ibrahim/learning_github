b3aba6685a03840257d46e8288e658cadc3b30cf
Faraz Ibrahim <fribrahim16@gmail.com>
2025-09-04 21:10:58 +0500

    new changes

---
+++ day1/hello.py
@@ -0,0 +1,10 @@
+
+
+age_at_school = 5
+faraz_age = input("How old is faraz? ") #input function #False because it is string by default we have to convert it into int as done in next step
+faraz_age=int(faraz_age)
+print(type(faraz_age))
+print(faraz_age==age_at_school) #logical operator
+
+
+
